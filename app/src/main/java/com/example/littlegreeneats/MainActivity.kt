d1483834dea1bd88c40c548241320dea0e410509
fritzthecrisp <zephteoys@gmail.com>
2022-03-06 23:59:41 +0800

    Integrated Firebase partially (no buttons/functions that interact with it)

--- app/src/main/java/com/example/littlegreeneats/MainActivity.kt
+++ app/src/main/java/com/example/littlegreeneats/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.littlegreeneats
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import android.content.Intent as Intent1
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val loginBtn: Button = findViewById(R.id.loginBtn)
+        val signupPageBtn: TextView = findViewById(R.id.SignupPageBtn)
+        val usernameLogin: EditText = findViewById(R.id.usernameLogin);
+        val passwordLogin: EditText = findViewById(R.id.passwordLogin);
+        loginBtn.setOnClickListener {
+
+            if (usernameLogin.text.trim().isNotEmpty() && passwordLogin.text.trim().isNotEmpty()){
+                Toast.makeText(this, "Input Provided", Toast.LENGTH_LONG).show();
+
+            }else{
+                Toast.makeText(this, "Input Required", Toast.LENGTH_LONG).show();
+
+            }
+
+
+        }
+        signupPageBtn.setOnClickListener {
+            val intent = Intent1 ( this, SignupPageActivity::class.java)
+            startActivity(intent);
+            Toast.makeText(this, "clicked", Toast.LENGTH_LONG).show();
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/littlegreeneats/SignupPageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.littlegreeneats
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignupPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup_page)
+    }
+}
